c89d6f8345b563603f8a0e21851913c23e449d69
Dave Austin <d.austin_391@yahoo.com>
2020-11-07 21:06:28 -0500

    displays total for most recent order

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Order, Product
 
+
 def index(request):
-    context = {
-        "all_products": Product.objects.all()
-    }
+    context = {"all_products": Product.objects.all()}
     return render(request, "store/index.html", context)
+
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
@@ -13,4 +13,5 @@ def checkout(request):
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    context = {"quantity": quantity_from_form, "total": total_charge}
+    return render(request, "store/checkout.html", context)
